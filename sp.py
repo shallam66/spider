c60c18a3460ce4e3e3ec46f37ab2ee5372443212
xielan <xielan@staff.sina.com.cn>
2015-07-15 19:14:39 +0800

    0715

--- sp.py
+++ sp.py
@@ -5,6 +5,18 @@ import logging
 import sys
 import getopt
 
+
+class Spider(object):
+    def __init__(self, url, deep, thread=10, dbfile='./spyder.log', key=None, detal=9):
+        self.url = url
+        self.deep = deep
+        self.thread = thread
+        self.dbfile = dbfile
+        self.key = key
+        self.detal = detal
+
+    def 
+
 def help_message():
     print '\tspider.py usage:'
     print '\t\t-h, --help: print help messages.'
@@ -18,14 +30,39 @@ def help_message():
 
 def args_process(argvs):
     try:
-        options, args = getopt.getopt(argvs[1:], "hu:d:l:", ['help', 'thread=', 'dbfile=', 'selftest'])
+        options, args = getopt.getopt(argvs[1:], "hu:d:l:", ['help',
+                                      'thread=', 'dbfile=', 'selftest'])
     except Exception, e:
         sys.exit()
-
+    op_get = {'detal': 9, 'thread': 10, 'dbfile': './spyder.log',
+              'key': None, 'selftest': False}
     for opt, values in options:
         if opt in ['-h', '--help']:
             help_message()
-
+            return None
+        elif opt == '-u':
+            op_get['url'] = values
+        elif opt == '-d':
+            op_get['deep'] = values
+        elif opt == '-l':
+            op_get['detal'] = values
+        elif opt == '--thread':
+            op_get['thread'] = values
+        elif opt == '--dbfile':
+            op_get['dbfile'] = values
+        elif opt == '--key':
+            op_get['key'] = values
+        elif opt == 'selftest':
+            op_get['selftest'] = True
+    if not op_get.has_key('url'):
+        raise RuntimeError('Please provide url!')
+    if not op_get.has_key('deep'):
+        raise RuntimeError('Please provide deep to spyder')
+    return op_get
 
 if  __name__ == '__main__':
-    args_process(sys.argv)
+    option = args_process(sys.argv)
+    if option is not None:
+        spider = Spider(option['url'], option['deep'], option['thread'],
+                        option['dbfile'], option['key'], option['detal'])
+        spider.start()
